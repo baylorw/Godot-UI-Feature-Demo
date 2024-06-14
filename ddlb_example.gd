6422638c3fbfb99141ee5348992d0f6220960572
baylor wetzel <baylorw@yahoo.com>
2024-06-14 12:23:09 -0500

    initial commit

---
+++ ddlb_example.gd
@@ -0,0 +1,57 @@
+extends Control
+
+@export var dropdown_path : NodePath
+@onready var dropdown : OptionButton = get_node(dropdown_path)
+
+var fruits = ["Apple", "Orange", "Banana", "Lemon"]
+
+func _ready():
+	dropdown.clear()
+	#add_items()
+	add_fruits()
+	#remove_item(1)
+	#disable_item(1)
+	### You can also rename with set_item_text(id,name)
+	dropdown.select(3)
+	
+	#dropdown.connect("item_selected", self, "on_item_changed")
+	
+func add_items():
+	dropdown.add_item("")
+	dropdown.add_separator()
+	dropdown.add_item("A")
+	dropdown.add_item("b")
+	dropdown.add_item("Si")
+
+func add_fruits():
+	#for item in fruits:
+		#dropdown.add_item(item + " $1")
+	for i in fruits.size():
+		var id = 10 * i
+		dropdown.add_item(fruits[i] + " $" + str(i), id)
+		var props = {
+			"name": fruits[i],
+			"id": id,
+			"price": i
+		}
+		dropdown.set_item_metadata(i, props)
+
+## Note: This re-indexes all the items in the list. There's never a gap in the indices.
+func remove_item(id):
+	dropdown.remove_item(id)
+
+func disable_item(id):
+	dropdown.set_item_disabled(id, true)
+
+func _on_dropdown_item_selected(index):
+	var idx2 = dropdown.get_selected_id()
+	dropdown.get_selected_metadata()
+	dropdown.selected
+	print("Selected ID=" + str(idx2))
+	var id = dropdown.get_item_id(index)
+	var props = dropdown.get_item_metadata(idx2)
+	print("Picked " + dropdown.get_item_text(index) + " index=" + str(index) + " id=" + str(id) + " object=" + fruits[index] + " props=" + str(props))
+	
+#func on_item_changed(id):
+	#print("manual event. Picked " + dropdown.get_item_text(id))
+	
---
+++ layout_example.gd
@@ -0,0 +1,9 @@
+extends Control
+
+###
+### 1. Create Control
+### 2. Add ColorRect
+### 3. Anchor = Full Rect
+### 4. Add MarginContainer (to the color?), make it Full Rect
+###
+
