ba1e879cff73a391c95534f39390218ab8961bac
baylor wetzel <baylorw@yahoo.com>
2024-06-14 17:01:26 -0500

    Added Panels example
    - Added art from Godot's official UI project, HoMM IV and my old AI Game.
    - Added Panels example.
    - Stubbed in Buttons example.

--- layout_example.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-###
-### 1. Create Control
-### 2. Add ColorRect
-### 3. Anchor = Full Rect
-### 4. Add MarginContainer (to the color?), make it Full Rect
-###
-
--- panel_example.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-
-### Add panel
-### Create a new Theme, import from Panel theme? Huh?
-### Control -> Theme -> Panel -> Styles -> Panel = StyleBoxTexture
-### Drag a dialog box image to Texture
-### Sub-Region -> Edit Region to set the borders
